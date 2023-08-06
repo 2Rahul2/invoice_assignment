2a527d4ea7f1322dad4a666a975250488a8a55c5
2Rahul2 <96227426+2Rahul2@users.noreply.github.com>
2023-08-06 23:25:58 +0530

    iews.py

--- invoice/app/views.py
+++ invoice/app/views.py
@@ -4,20 +4,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Invoice ,InvoiceDetail
 from .serializers import InvoiceSerializer ,InvoiceDetailSerializer
-
-@api_view(['GET', 'POST'])
-def invoice_list(request):
-    if request.method == 'GET':
-        invoices = Invoice.objects.all()
-        serializer = InvoiceSerializer(invoices, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = InvoiceDetailSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
     
 def homePage(request):
     return render(request , 'app/index.html')
